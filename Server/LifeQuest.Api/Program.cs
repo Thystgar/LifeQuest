69c0daf178c125845dbb7be1f41c2193ce0fbd0d
Jana Komarkova <jana.komarkova@outlook.com>
2025-05-19 20:16:57 +0100

    refactor Program

--- Server/LifeQuest.Api/Program.cs
+++ Server/LifeQuest.Api/Program.cs
@@ -1,58 +1,74 @@
 using LifeQuest.Api.Processors;
 using LifeQuest.Api.Storage;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace LifeQuest.Api
 {
-	public class Program
+	public partial class Program
 	{
 		public static void Main( string[] args )
 		{
-			var builder = WebApplication.CreateBuilder();
-
-			// Add services to the container.
-			RegisterServices(builder.Services, builder.Configuration);
-
+            var builder = CreateHostBuilder(args);
 
 			var app = builder.Build();
-
-			ConfigureApplication(app);
 
 			app.Run();
 		}
 
-		public static void RegisterServices(IServiceCollection services, IConfiguration config)
-        {
-            var connectionString = config.GetValue<string>("Database:ConnectionString");
+        public static IHostBuilder CreateHostBuilder(string[] args){
+            var builder = new HostBuilder();
 
-            services.AddControllers();
-			services.AddDbContext<LifeQuestContext>(options => options.UseSqlServer(connectionString));
-			services.AddScoped<IQuestStorage, QuestDbStorage>();
-			services.AddScoped<IRewardStorage, RewardDbStorage>();
-			services.AddScoped<IAccountStorage, AccountDbStorage>();
-			services.AddScoped<IAccountProcessor, AccountProcessor>();
-			services.AddScoped<IRewardProcessor, RewardProcessor>();
-			services.AddScoped<IQuestProcessor, QuestProcessor>();
+            builder
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var env = context.HostingEnvironment.EnvironmentName;
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    config.AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    var connectionString = context.Configuration.GetValue<string>("Database:ConnectionString");
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
-		}
+                    services.AddControllers();
+                    services.AddDbContext<LifeQuestContext>(options => options.UseSqlServer(connectionString));
+                    services.AddScoped<IQuestStorage, QuestDbStorage>();
+                    services.AddScoped<IRewardStorage, RewardDbStorage>();
+                    services.AddScoped<IAccountStorage, AccountDbStorage>();
+                    services.AddScoped<IAccountProcessor, AccountProcessor>();
+                    services.AddScoped<IRewardProcessor, RewardProcessor>();
+                    services.AddScoped<IQuestProcessor, QuestProcessor>();
 
-		public static WebApplication ConfigureApplication(WebApplication app)
-		{
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
+                    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                    services.AddEndpointsApiExplorer();
+                    services.AddSwaggerGen();
+                })
+                .ConfigureWebHost(builder =>
+                {
+                    builder
+                    .Configure((context, app) =>
+                    {
+                        var env = context.HostingEnvironment;
 
-			app.UseHttpsRedirection();
-			app.UseAuthorization();
-			app.MapControllers();
+                        if (env.IsDevelopment())
+                        {
+                            app.UseSwagger();
+                            app.UseSwaggerUI();
+                        }
 
-			return app;
-		}
-	}
+                        app.UseHttpsRedirection();
+                        app.UseRouting();
+                        app.UseAuthorization();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapControllers();
+                        });
+                    })
+                    .UseKestrel();
+                });
+                
+            return builder;
+        }
+    }
 }
