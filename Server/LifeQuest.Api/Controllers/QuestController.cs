e5e8c4696149f316306a4c36bfb9139e4329e5a5
Jana Komarkova <jana.komarkova@outlook.com>
2025-07-02 22:25:27 +0100

    Add quest logs

--- Server/LifeQuest.Api/Controllers/QuestController.cs
+++ Server/LifeQuest.Api/Controllers/QuestController.cs
@@ -1,6 +1,7 @@
 using LifeQuest.Api.Models.API;
 using LifeQuest.Api.Processors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace LifeQuest.Api.Controllers
 {
@@ -9,16 +10,19 @@ namespace LifeQuest.Api.Controllers
     public class QuestController : ControllerBase
     {
         private readonly IQuestProcessor _questProcessor;
+        private readonly ILogger<QuestController> _logger;
         private const string USER = "userIdTBD"; // TODO: Replace with actual user ID retrieval logic
 
-        public QuestController(IQuestProcessor questProcessor)
+        public QuestController(IQuestProcessor questProcessor, ILogger<QuestController> logger)
         {
             _questProcessor = questProcessor;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<QuestApiModel>>> GetQuestsAsync()
         {
+            _logger.LogInformation("Getting all quests for user {UserId}", USER);
             var quests = await _questProcessor.GetQuestsAsync();
             return Ok(quests);
         }
