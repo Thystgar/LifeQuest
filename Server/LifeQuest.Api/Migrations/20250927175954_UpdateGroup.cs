9c96693de6b0d23b327d0b715572c97ebd04e7b4
Jana Komarkova <jana.komarkova@outlook.com>
2025-09-27 19:40:41 +0100

    make account processor retrieve user context on its own

---
+++ Server/LifeQuest.Api/Migrations/20250927175954_UpdateGroup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LifeQuest.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Code",
+                table: "Groups",
+                newName: "InviteCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Groups",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Groups");
+
+            migrationBuilder.RenameColumn(
+                name: "InviteCode",
+                table: "Groups",
+                newName: "Code");
+        }
+    }
+}
